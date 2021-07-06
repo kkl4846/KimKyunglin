b41cd85a04c620c789a1d98918924748f56d841b
kkl4846 <kkl4846@cau.ac.kr>
2021-07-06 22:50:35 +0900

    1-6

--- python_problem/python-problem.py
+++ python_problem/python-problem.py
@@ -1,37 +1,53 @@
 num=0
+finish='0'
 while True:
-    numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
-    try:
-        numberOfInput=int(numberOfInput)
-        it_is=True
-    except ValueError:
-        it_is=False
-    if it_is==False:
-        print('정수를 입력하세요')
-    else:
-        if 1<=numberOfInput<=3:
-            break
+    while True:
+        numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
+        try:
+            numberOfInput=int(numberOfInput)
+            it_is=True
+        except ValueError:
+            it_is=False
+        if it_is==False:
+            print('정수를 입력하세요')
         else:
-            print('1,2,3 중 하나를 입력하세요')
+            if 1<=numberOfInput<=3:
+                break
+            else:
+                print('1,2,3 중 하나를 입력하세요')
 
-for i in range(numberOfInput):
-    num=num+1
-    print('playerA :',num)
-
-while True:
-    numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
-    try:
-        numberOfInput=int(numberOfInput)
-        it_is=True
-    except ValueError:
-        it_is=False
-    if it_is==False:
-        print('정수를 입력하세요')
-    else:
-        if 1<=numberOfInput<=3:
+    for i in range(numberOfInput):
+        num=num+1
+        print('playerA :',num)
+        if num==31:
+            finish='B'
             break
+    if finish!='0':
+
+        break
+            
+
+    while True:
+        numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
+        try:
+            numberOfInput=int(numberOfInput)
+            it_is=True
+        except ValueError:
+            it_is=False
+        if it_is==False:
+            print('정수를 입력하세요')
         else:
-            print('1,2,3 중 하나를 입력하세요')
-for i in range(numberOfInput):
-    num=num+1
-    print('playerB :',num)
+            if 1<=numberOfInput<=3:
+                break
+            else:
+                print('1,2,3 중 하나를 입력하세요')
+    for i in range(numberOfInput):
+        num=num+1
+        print('playerB :',num)
+        if num==31:
+            finish='A'
+            break
+    if finish!='0':
+        
+        break
+            
