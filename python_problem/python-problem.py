382bd20f35379d16b53047cc9dd0d669eda7366e
kkl4846 <kkl4846@cau.ac.kr>
2021-07-06 22:41:43 +0900

    1-5

--- python_problem/python-problem.py
+++ python_problem/python-problem.py
@@ -17,3 +17,21 @@ while True:
 for i in range(numberOfInput):
     num=num+1
     print('playerA :',num)
+
+while True:
+    numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
+    try:
+        numberOfInput=int(numberOfInput)
+        it_is=True
+    except ValueError:
+        it_is=False
+    if it_is==False:
+        print('정수를 입력하세요')
+    else:
+        if 1<=numberOfInput<=3:
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+for i in range(numberOfInput):
+    num=num+1
+    print('playerB :',num)
