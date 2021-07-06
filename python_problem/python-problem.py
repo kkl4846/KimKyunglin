26326e143467f55f43421393987425d43c963cf3
kkl4846 <kkl4846@cau.ac.kr>
2021-07-06 22:36:19 +0900

    1-3_modified_version

--- python_problem/python-problem.py
+++ python_problem/python-problem.py
@@ -1,13 +1,15 @@
 num=0
 while True:
     numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
-    if numberOfInput.isdigit():
-        if numberOfInput.isdigit():
-            if numberOfInput!=1 or numberOfInput!=2 or numberOfInput!=3:
-                print('1,2,3 중 하나를 입력하세요')
-            else:
-                break
-        else:
-            print('정수를 입력하세요')
+    try:
+        numberOfInput=int(numberOfInput)
+        it_is=True
+    except ValueError:
+        it_is=False
+    if it_is==False:
+        print('정수를 입력하세요')
     else:
-         print('정수를 입력하세요')
+        if 1<=numberOfInput<=3:
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요')
