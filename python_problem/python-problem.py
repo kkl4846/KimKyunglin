f1ad092e197a0e1420172c4537f47ee29cb04313
kkl4846 <kkl4846@cau.ac.kr>
2021-07-06 23:08:40 +0900

    1-8

--- python_problem/python-problem.py
+++ python_problem/python-problem.py
@@ -1,53 +1,40 @@
+
 num=0
 finish='0'
+def brGame(user):
+        global num,finish
+        while True:
+            numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
+            try:
+                numberOfInput=int(numberOfInput)
+                it_is=True
+            except ValueError:
+                it_is=False
+            if it_is==False:
+                print('정수를 입력하세요')
+            else:
+                if 1<=numberOfInput<=3:
+                    break
+                else:
+                    print('1,2,3 중 하나를 입력하세요')
+
+        for i in range(numberOfInput):
+            num=num+1
+            print('player{0} :{1}'.format(user,num))
+            if num==31:
+                if user=='A':
+                    finish='B'
+                else:
+                    finish='A'
+                break
+
+
 while True:
-    while True:
-        numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
-        try:
-            numberOfInput=int(numberOfInput)
-            it_is=True
-        except ValueError:
-            it_is=False
-        if it_is==False:
-            print('정수를 입력하세요')
-        else:
-            if 1<=numberOfInput<=3:
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-
-    for i in range(numberOfInput):
-        num=num+1
-        print('playerA :',num)
-        if num==31:
-            finish='B'
-            break
+    brGame('A')
     if finish!='0':
         print('player{0} win!'.format(finish))
         break
-            
-
-    while True:
-        numberOfInput=input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :')
-        try:
-            numberOfInput=int(numberOfInput)
-            it_is=True
-        except ValueError:
-            it_is=False
-        if it_is==False:
-            print('정수를 입력하세요')
-        else:
-            if 1<=numberOfInput<=3:
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-    for i in range(numberOfInput):
-        num=num+1
-        print('playerB :',num)
-        if num==31:
-            finish='A'
-            break
+    brGame('B')
     if finish!='0':
         print('player{0} win!'.format(finish))
         break
-            
