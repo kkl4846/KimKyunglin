89338bc5c93f06c6a6f36ccdd2decb614b2a89cd
kkl4846 <kkl4846@cau.ac.kr>
2021-07-13 23:22:46 +0900

    애플리케이션 확장하기

--- Djangogirls/myvenv/blog/urls.py
+++ Djangogirls/myvenv/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+      path('post/<int:pk>/', views.post_detail, name='post_detail'), #post/<int:pk/>/는 url 패턴을 나타냄/ post/1 post/2 ..
 ]
