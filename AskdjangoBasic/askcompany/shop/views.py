9dbd87a95200adc75ec5d29ee524c358dd9dd7ff
kkl4846 <kkl4846@cau.ac.kr>
2021-07-15 22:47:59 +0900

    07강

--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -1,19 +1,53 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.http.response import Http404
+from django.shortcuts import render, redirect , resolve_url, get_object_or_404
 from shop.models import Item
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
-#06강- view 호출 시, 인자
-#1번째 인자: Httprequest 객체 / 현재 요청에 대한 모든 내역을 담고 있다
-#2번째~ 인자: 현재 요청의 url로부터 capture된 문자열들
-#url/re_path 를 통한 처리에서는 -> 모든 인자는 str타입으로 전달
-#path를 통한 처리에서는 매핑된 converter의 to_python에 맞게 변환된 값이 인자로 전달
 
-#return값은 HttpResponse의 객체이어야한다.
+#07강 http 상태코드로 응답
+#200번대: 성공 300번대: 요청을 마치기 위해ㅣ 추가동작 400번대: 클라이언트측 오류 500번대 서버측 오류(indexerror,keyerror,django,db,models.objectdoesnotexist등)
 
+#200번대 
+def view1(request):
+    return HttpResponse('Hello, Ask Company')
+def view2(request):
+    return render(request, 'template.html')
+def view3(request):
+    return JsonResponse({'hello': 'Ask Company'}) #객체를 문자열로 변환한다음 return 해야하는데 이때 response를 사용하면 내부적으로 변환해줌
 
-#request.method --> GET ,POST인지 알 수 있다.
-#request.META 메타 정보를 알 수 있다.
+#302 응답하는 예시
+def view1(request):
+    return HttpResponseRedirect('/shop/')
+
+def view2(request):
+    url=resolve_url('shop:item_list')
+    return HttpResponseRedirect(url)
+def view3(request):                         #view2 코드를 한줄로
+    return redirect('shop:item_list')
+
+#404 응답하는 예시  try except를 통해 item이 없을 때 오류를 잡을 수 있다. or get_object_or_404의 조건을 통해
+def view1(request):  
+    try:
+        item=Item.objects.get(pk=100)
+    except Item.DoesNotExist:
+        raise Http404
+def view2(request):
+    item=get_object_or_404(Item,pk=100)
+def view3(request):
+    try:
+        item-Item.objects.get(pk=100)
+    except Item.DoesNotExist:
+        return HttpResponseNotFound()
+
+#500 응답하는 에시
+def view1(request):
+    name=['Tom','Steve'][100] #idex error
+
+    item=Item.objects.get(pk=100) # item레코드가 없거나 1개 있을 때 dosenotExist 예외  그이상이면 multipleObjectsReturn
+
 def archives_year(request,year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
 
