5533ba29cf08064eaff9c43604db0e9a91eef539
kkl4846 <kkl4846@cau.ac.kr>
2021-07-16 01:01:12 +0900

    08강 장고 쉘

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+#08강에서 shell 실행하려면 Item객체 생성해줘야해서 추가
+class Item(models.Model):
+    name=models.CharField(verbose_name='이름',max_length=100)
+    def __str__(self):
+        return self.name
